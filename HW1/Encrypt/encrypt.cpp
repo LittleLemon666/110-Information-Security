0eb153418b3abbeb0b5aa7fb8dcc471b61743c25
FireramaRalphadel <94827066+b10815001@users.noreply.github.com>
2022-03-16 23:01:50 +0800

    print for linux

--- HW1/Encrypt/encrypt.cpp
+++ HW1/Encrypt/encrypt.cpp
@@ -69,6 +69,7 @@ string playfair(string input, string key)
 string vernam(string input, string key)
 {
 	key += input;
+	cout << key << '\n';
 	string result = "";
 	for (int id = 0; id < input.length(); id++)
 	{
