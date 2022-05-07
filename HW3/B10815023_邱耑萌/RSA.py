bd2a6cb9a2a4890def105bd7c56da8706d09e7bc
LittleLemon666 <damoncho510@gmail.com>
2022-05-07 15:12:46 +0800

    Implemented Miller Rabin Test

--- HW3/B10815023_邱耑萌/RSA.py
+++ HW3/B10815023_邱耑萌/RSA.py
@@ -8,3 +8,31 @@ def doSquareAndMultiply(x, H, n):
     return y
 
 #print(doSquareAndMultiply(2, 32, 5487)) #3536
+
+def doMillerRabinTest(x):
+    if x % 2 == 0:
+        return False
+    
+    test_x = x - 1
+    m = 1
+    for k in range(1, 1024):
+        if test_x % (2 ** k) == 0:
+            m = test_x // (2 ** k)
+            break
+        
+    for a in range(2, test_x):
+        b = doSquareAndMultiply(a, m, x)
+        if b != 1 and b != test_x:
+            i = 1
+            while i < k and b != test_x:
+                b = b ** 2 % x
+                if b == 1:
+                    return False
+                i += 1
+            if b != test_x:
+                return False
+    return True
+
+# for i in range(3, 100):
+#     if doMillerRabinTest(i):
+#         print(i)
