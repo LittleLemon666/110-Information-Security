8b3ce483de1d2211951d27d376d6ea83a3ef9050
LittleLemon666 <damoncho510@gmail.com>
2022-05-07 16:06:13 +0800

    generate p & q

--- HW3/B10815023_邱耑萌/RSA.py
+++ HW3/B10815023_邱耑萌/RSA.py
@@ -1,3 +1,6 @@
+import argparse
+import random
+
 def doSquareAndMultiply(x, H, n):
     y = 1
     H = bin(int(str(H), base=10))[2:]
@@ -20,19 +23,53 @@ def doMillerRabinTest(x):
             m = test_x // (2 ** k)
             break
         
-    for a in range(2, test_x):
-        b = doSquareAndMultiply(a, m, x)
-        if b != 1 and b != test_x:
-            i = 1
-            while i < k and b != test_x:
-                b = b ** 2 % x
-                if b == 1:
-                    return False
-                i += 1
-            if b != test_x:
+    a = random.randint(2, test_x)
+    b = doSquareAndMultiply(a, m, x)
+    if b != 1 and b != test_x:
+        i = 1
+        while i < k and b != test_x:
+            b = b ** 2 % x
+            if b == 1:
                 return False
+            i += 1
+        if b != test_x:
+            return False
     return True
 
 # for i in range(3, 100):
 #     if doMillerRabinTest(i):
 #         print(i)
+
+def generateRandomKey(bit):
+    x = ''
+    for i in range(bit):
+        x += random.choice(["0", "1"])
+    return x
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--generateKey", action="store_true",
+						help="generate Keys")
+
+    args = parser.parse_args()
+
+    if args.generateKey:
+        p = ''
+        q = ''
+        p_int = 0
+        q_int = 0
+        while True:
+            p = generateRandomKey(512)
+            p_int = int(str(p), base=2)
+            if doMillerRabinTest(p_int):
+                break
+        while True:
+            q = generateRandomKey(512)
+            q_int = int(str(q), base=2)
+            if doMillerRabinTest(q_int):
+                break
+        
+        n_int = p_int * q_int
+        print("p = " + str(p_int))
+        print("q = " + str(q_int))
+        print("N = " + str(n_int))
